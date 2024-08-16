7c601961eea3cf2d020a3200ff944e75d6477b78
szprivitzky <szprivitzky@gmail.com>
2024-08-16 14:54:15 +0200

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,15 @@
+szam1=int(input("Kérek egy számot:"))
+szam2=int(input("Kérek még 1-et:"))
+oszto=0
+
+while(not(szam1==oszto or szam2==oszto)):
+    oszto=oszto + 1
+print(oszto)    
+
+
+
+
+
+    
+
+
