1762f8592cc37e127276c45b15876e7033b6a79e
szprivitzky <szprivitzky@gmail.com>
2024-08-17 00:28:34 +0200

    Add files via upload

---
+++ megszolítás.py
@@ -0,0 +1,33 @@
+name=str(input("Kérem a nevét: "))
+nem=str(input("Kerem a nemet(F/N):"))
+if(nem=="F" or "f"):
+    nem=" Úr "
+    print(name + nem)
+else:
+    nem=" Asszony "
+    print(name + nem)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
