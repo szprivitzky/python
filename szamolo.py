4da9554fa137dd17be2b441210dcf3d141a5935d
szprivitzky <szprivitzky@gmail.com>
2024-08-16 17:04:03 +0200

    Add files via upload

---
+++ szamolo.py
@@ -0,0 +1,24 @@
+from math import *
+
+
+a=int(input("Kérek egy számot: "))
+b=int(input("Kérek egy számot: "))
+c=int(input("Kérek egy számot: "))
+k=a+b+c
+d=(a+b+c)/2
+t= sqrt(d*(d-a)*(d-b)*(d-c))
+print(t,k)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
