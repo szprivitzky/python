7ce2617d5768613e52185d0cd4e79e33dc2ba809
szprivitzky <szprivitzky@gmail.com>
2024-08-20 20:33:25 +0200

    Add files via upload

---
+++ dobozterfogat.py
@@ -0,0 +1,33 @@
+x1=input(str("Kérek egy számot:"))
+x2=input(str("Kérek egy számot:"))
+x3=input(str("Kérek egy számot:"))
+def dobozTerfogat(x1,x2,x3):
+    return int(x1)*int(x2)*int(x3)
+print (dobozTerfogat(x1,x2,x3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
