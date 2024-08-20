441615af007f159f46815593ee8b5149c422ac52
szprivitzky <szprivitzky@gmail.com>
2024-08-20 16:56:36 +0200

    Add files via upload

---
+++ nca.py
@@ -0,0 +1,37 @@
+ca=input(str("Kérek 2-t betut:"))
+n=input(str("Kérek egy számot:"))
+n2=int(n)
+
+
+def karaktersor(n,ca):
+    "Ez a függvény összeszorozza az 'n'-t és a 'ca'-t"
+    print(int(n2)*ca)
+
+karaktersor(n,ca)
+
+print(karaktersor.__doc__)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
