c301c286549cdd850116fa494297ad49796269d6
szprivitzky <szprivitzky@gmail.com>
2024-08-21 17:33:25 +0200

    Add files via upload

---
+++ negyzet.py
@@ -0,0 +1,32 @@
+from turtle import *
+
+
+def negyzet(meret,szin):
+    color(szin)
+    i=0
+    while (i<4):
+        forward(meret)
+        right(90)
+        i=i+1
+
+def haromszog(meret,szin2):
+    color(szin2)
+    z=0
+    while(z==0):
+        forward(meret)
+        right(120)
+        forward(meret)
+        right(120)
+        forward(meret)
+        right(120)
+
+        z=z+1
+
+
+
+
+
+
+
+
+
